c5de8ba3c90b2ce44059a2e0a1d1bf0502454e93
francescobonaldo <39643021+francescobonaldo@users.noreply.github.com>
2018-06-29 17:25:01 +0200

    Primo commit

---
+++ Ema_Project/Ema_Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ema_Project
+{
+    class Program
+    {
+        List<string> listSongs = new List<string>();
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("we, benvenuto, scrivi il testo che cerchi");
+            search(Console.ReadLine());
+            Console.ReadKey();
+        }
+
+        public void search(string s)
+        {
+            //listSongs.Exists(a => a )
+            //bool a = 
+            //if(listSongs.Exists(a=>a = s))
+            //{
+
+            //}
+        }
+    }
+}
